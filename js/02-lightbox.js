52761744264cae92700f231b2ff02be7b950f686
Cosmits <salge.cos@gmail.com>
2023-04-21 13:58:39 +0200

    task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryUlEl = document.querySelector('.gallery');
+galleryUlEl.insertAdjacentHTML('beforeend', doGalleryList(galleryItems));
+
+// Initialize SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    animationSpeed: 250,
+});
+
+
+//
+//~ any functions 
+//
+function doGalleryList(arrGalleryItems) {
+    return arrGalleryItems.map(({ preview, original, description }) => {
+        //const { preview, origin, description } = el;
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+    </li>`
+    }).join('');
+}
